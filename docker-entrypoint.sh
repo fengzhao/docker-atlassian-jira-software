a198f550d384a6b86b512447b96ee9af18e2ccd8
fengzhao <fengzhao1124@hotmail.com>
2021-03-29 11:46:22 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,10 @@
 # check if the `server.xml` file has been changed since the creation of this
 # Docker image. If the file has been changed the entrypoint script will not
 # perform modifications to the configuration file.
+
+# 查看文件最近修改的Unix时间戳（从计算机纪元时间到最近一次修改的秒数）
+# https://www.cnblogs.com/Jack47/p/unix-time-date-episode.html
+
 if [ "$(stat -c "%Y" "${JIRA_INSTALL}/conf/server.xml")" -eq "0" ]; then
   if [ -n "${X_PROXY_NAME}" ]; then
     xmlstarlet ed --inplace --pf --ps --insert '//Connector[@port="8080"]' --type "attr" --name "proxyName" --value "${X_PROXY_NAME}" "${JIRA_INSTALL}/conf/server.xml"
